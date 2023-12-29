688cf192d0037b3263e1cbd45a3881d86f61781a
codecrush0318 <mateo.castro@mercurydasha.com>
2023-12-29 11:24:40 -0500

    Update Bot

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require("node-telegram-bot-api");
 
-const token = "6939617736:AAGwQhYPjBz3efc52Yi1L_tDBy6VM3tyJkI";
+const token = "6771631978:AAH0sGO2XaMb6ss8bGjpqXGbAuBLP2QcYuk";
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", (msg) => {
@@ -8,7 +8,7 @@ bot.on("message", (msg) => {
   const messageText = msg.text;
 
   if (messageText === "/start") {
-    const gameUrl = "t.me/candy_tg_game_bot/duck_playing_game";
+    const gameUrl = "t.me/tg_games_new_bot/tgapp";
     const keyboard = {
       inline_keyboard: [
         [
